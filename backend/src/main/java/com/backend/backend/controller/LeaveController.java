7097571e786969a3db620797346f6f59ddc29653
Nyi Nyi Thu <nyithu185@gmail.com>
2024-02-10 13:01:22 +0630

    add leave controller

--- backend/src/main/java/com/backend/backend/controller/LeaveController.java
+++ backend/src/main/java/com/backend/backend/controller/LeaveController.java
@@ -1,10 +1,16 @@
 package com.backend.backend.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +29,25 @@ public class LeaveController {
 	@ResponseBody
 	public int addLeave(@ModelAttribute LeaveModel leaveModel) {
 		return leaveServiceImpl.addLeave(leaveModel);
+	}
+	
+	@DeleteMapping(value = "deleteLeave", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public int deleteLeave(@PathVariable (name = "leave_id")String leave_id) {
+		int leaveId = Integer.parseInt(leave_id);
+		return leaveServiceImpl.deleteLeave(leaveId);
+	}
+	
+	@GetMapping(value = "leaveList", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public List<LeaveModel> leaveList(){
+		return leaveServiceImpl.leaveList();
+	}
+	
+	@PutMapping(value = "editLeave", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public int editLeave(@RequestBody LeaveModel leaveModel) {
+		return leaveServiceImpl.editLeave(leaveModel);
 	}
 
 }
