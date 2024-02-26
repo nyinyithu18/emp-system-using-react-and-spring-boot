6c08e0fc01eb0f9d532fd0b7d7afa563c979327d
nyinyithu18 <nyithu185@gmail.com>
2024-02-26 17:28:09 +0630

    add some codes for new features

---
+++ backend/src/main/java/com/backend/backend/service/EmpInterestService.java
@@ -0,0 +1,11 @@
+package com.backend.backend.service;
+
+import java.util.List;
+
+import com.backend.backend.model.EmpModel;
+
+public interface EmpInterestService {
+
+	public int addEmpInterest(EmpModel empModel);
+	public List<EmpModel> empInterestList();
+}
