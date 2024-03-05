dede5980f70f8d37ca5cce697987ca914a5f980c
nyinyithu18 <nyithu185@gmail.com>
2024-03-05 09:30:46 +0630

    add image at pdf export from backend

--- backend/src/main/java/com/backend/backend/service/empserviceimpl/EmpServiceImpl.java
+++ backend/src/main/java/com/backend/backend/service/empserviceimpl/EmpServiceImpl.java
@@ -1,8 +1,15 @@
 package com.backend.backend.service.empserviceimpl;
 
+import java.awt.AlphaComposite;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.geom.Ellipse2D;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.util.List;
+
+import javax.imageio.ImageIO;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,26 +22,29 @@ import com.backend.backend.service.EmpService;
 import com.lowagie.text.Chunk;
 import com.lowagie.text.Document;
 import com.lowagie.text.DocumentException;
+import com.lowagie.text.Element;
 import com.lowagie.text.Font;
 import com.lowagie.text.FontFactory;
+import com.lowagie.text.Image;
 import com.lowagie.text.PageSize;
 import com.lowagie.text.Paragraph;
+import com.lowagie.text.Rectangle;
+import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfWriter;
 
-
 @Service
-public class EmpServiceImpl implements EmpService{
+public class EmpServiceImpl implements EmpService {
 
 	@Autowired
 	private EmpRepository empRepository;
-	
+
 	@Autowired
 	private LeaveRepository leaveRepository;
-	
+
 	@Autowired
 	private EmpInterestRepository empInterestRepository;
-	
+
 	@Override
 	public int addEmp(EmpModel empModel) {
 		return empRepository.addEmp(empModel);
@@ -71,110 +81,156 @@ public class EmpServiceImpl implements EmpService{
 		Document document = new Document(PageSize.A4);
 		try {
 			PdfWriter.getInstance(document, outputStream);
-			
+
 			document.open();
-			
-			
-			
+
 			EmpModel employee = empRepository.searchById(emp_id);
-			if(employee != null) {
-				
+			if (employee != null) {
+
 				Font fontTitle = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
 				fontTitle.setSize(18);
-				
-				Paragraph title = new Paragraph("Employee Details \n", fontTitle);
+
+				Paragraph title = new Paragraph("Employee Details \n" + "\n", fontTitle);
 				title.setAlignment(Paragraph.ALIGN_CENTER);
 				document.add(title);
-				
+
+				PdfPTable table = new PdfPTable(2); // 2 columns
+				table.setWidthPercentage(100);
+				table.setSpacingBefore(10f);
+				table.setSpacingAfter(10f);
+
+				PdfPCell detailsCell = new PdfPCell();
+				detailsCell.setBorder(Rectangle.NO_BORDER);
+				detailsCell.setHorizontalAlignment(Element.ALIGN_RIGHT);
+				detailsCell.setVerticalAlignment(Element.ALIGN_TOP);
+
 				Font fontContent = FontFactory.getFont(FontFactory.HELVETICA);
 				fontContent.setSize(12);
-				
-				
-		        Paragraph empDetails = new Paragraph();
-		        empDetails.setFont(fontContent);
 
-		        empDetails.add("Employee ID >> " + employee.getEmp_id() + "\n" + "\n");
-		        empDetails.add("Name            >> " + employee.getEmp_name() + "\n" + "\n");
-		        empDetails.add("NRC              >> " + employee.getNrc() + "\n" + "\n");
-		        empDetails.add("Phone           >> " + employee.getPhone() + "\n" + "\n");
-		        empDetails.add("Email            >> " + employee.getEmail() + "\n" + "\n");
-		        empDetails.add("DOB             >> " + employee.getDob() + "\n" + "\n");
-		        empDetails.add("Rank             >> " + employee.getRank() + "\n" + "\n");
-		        empDetails.add("Department  >> " + employee.getDep() + "\n" + "\n");
-		        empDetails.add("Address        >> " + employee.getAddress() + "\n");
+				Paragraph empDetails = new Paragraph();
+				empDetails.setFont(fontContent);
 
-		        document.add(empDetails);
-		        document.add(Chunk.NEWLINE); 
-		        
-		        List<EmpModel> interests = empInterestRepository.findInterestsByEmployeeId(emp_id);
-	            if (!interests.isEmpty()) {
-	                addInterestsData(document, interests);
-	            }
-		        
-		        List<EmpModel> leaveList = leaveRepository.findLeaveByEmployeeId(emp_id);
-	            if (!leaveList.isEmpty()) {
-	                addLeaveData(document, leaveList);
-	            }
+				empDetails.add("Employee ID  >>  " + employee.getEmp_id() + "\n" + "\n");
+				empDetails.add("Name             >>  " + employee.getEmp_name() + "\n" + "\n");
+				empDetails.add("NRC               >>  " + employee.getNrc() + "\n" + "\n");
+				empDetails.add("Phone            >>  " + employee.getPhone() + "\n" + "\n");
+				empDetails.add("Email             >>  " + employee.getEmail() + "\n" + "\n");
+				empDetails.add("DOB              >>  " + employee.getDob() + "\n" + "\n");
+				empDetails.add("Rank              >>  " + employee.getRank() + "\n" + "\n");
+				empDetails.add("Department   >>  " + employee.getDep() + "\n" + "\n");
+				empDetails.add("Address         >>  " + employee.getAddress() + "\n");
+
+				detailsCell.addElement(empDetails);
+				table.addCell(detailsCell);
+
+				PdfPCell imageCell = new PdfPCell();
+				imageCell.setBorder(Rectangle.NO_BORDER);
+				imageCell.setHorizontalAlignment(Element.ALIGN_LEFT);
+				imageCell.setVerticalAlignment(Element.ALIGN_TOP);
+
+				byte[] imageData = employee.getImage();
+				if (imageData != null) {
+					try {
+						BufferedImage bufferedImage = ImageIO.read(new ByteArrayInputStream(imageData));
+						int diameter = Math.min(bufferedImage.getWidth(), bufferedImage.getHeight());
+
+						// Create a BufferedImage with a circular mask
+						BufferedImage circularImage = new BufferedImage(diameter, diameter,
+								BufferedImage.TYPE_INT_ARGB);
+						Graphics2D g2 = circularImage.createGraphics();
+						g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+						g2.setComposite(AlphaComposite.Src);
+						g2.setClip(new Ellipse2D.Float(0, 0, diameter, diameter));
+						g2.drawImage(bufferedImage, 0, 0, diameter, diameter, null);
+						g2.dispose();
+
+						// Convert BufferedImage back to Image
+						Image circularPdfImage = Image.getInstance(circularImage, null);
+						circularPdfImage.scaleToFit(100, 100); // Adjust width and height as needed
+
+						circularPdfImage.setAlignment(Image.ALIGN_RIGHT);
+						circularPdfImage.scaleToFit(100, 100);
+						imageCell.addElement(circularPdfImage);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+				table.addCell(imageCell);
+
+				document.add(table);
+
+				List<EmpModel> interests = empInterestRepository.findInterestsByEmployeeId(emp_id);
+				if (!interests.isEmpty()) {
+					addInterestsData(document, interests);
+				}
+
+				List<EmpModel> leaveList = leaveRepository.findLeaveByEmployeeId(emp_id);
+				if (!leaveList.isEmpty()) {
+					addLeaveData(document, leaveList);
+				}
 			}
-	        
+
 		} catch (DocumentException e) {
 			e.printStackTrace();
 		} finally {
-			if(document != null) {
+			if (document != null) {
 				document.close();
 			}
 		}
-		
+
 		return outputStream.toByteArray();
-
 	}
-	
+
 	private void addInterestsData(Document document, List<EmpModel> interests) throws DocumentException {
-	    Font fontTitle = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
-	    fontTitle.setSize(14);
+		Font fontTitle = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
+		fontTitle.setSize(14);
 
-	    Paragraph interestsTitle = new Paragraph("Interests", fontTitle);
-	    interestsTitle.setAlignment(Paragraph.ALIGN_LEFT);
-	    document.add(interestsTitle);
+		Paragraph interestsTitle = new Paragraph("Interests", fontTitle);
+		interestsTitle.setAlignment(Paragraph.ALIGN_LEFT);
+		document.add(interestsTitle);
 
-	    // Create a bullet list for interests
-	    com.lowagie.text.List list = new com.lowagie.text.List(com.lowagie.text.List.UNORDERED);
-	    for (EmpModel interest : interests) {
-	        list.add(new com.lowagie.text.ListItem(interest.getInterest_name()));
-	    }
+		// Create a bullet list for interests
+		com.lowagie.text.List list = new com.lowagie.text.List(com.lowagie.text.List.UNORDERED);
+		for (EmpModel interest : interests) {
+			if (interest.isInterest_checked() == false) {
+				list.add(new com.lowagie.text.ListItem(interest.getInterest_name()));
+			}
+		}
 
-	    document.add(list);
-	    document.add(Chunk.NEWLINE);
+		document.add(list);
+		document.add(Chunk.NEWLINE);
 	}
-	
+
 	private void addLeaveData(Document document, List<EmpModel> leaveList) throws DocumentException {
-	    Font fontTitle = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
-	    fontTitle.setSize(14);
+		Font fontTitle = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
+		fontTitle.setSize(14);
 
-	    Paragraph leaveTitle = new Paragraph("Leave Data", fontTitle);
-	    leaveTitle.setAlignment(Paragraph.ALIGN_CENTER);
-	    document.add(leaveTitle);
+		Paragraph leaveTitle = new Paragraph("Leave Data", fontTitle);
+		leaveTitle.setAlignment(Paragraph.ALIGN_CENTER);
+		document.add(leaveTitle);
 
-	    PdfPTable table = new PdfPTable(4); // 4 columns 
-	    table.setWidthPercentage(100);
-	    table.setSpacingBefore(10f);
-	    table.setSpacingAfter(10f);
+		PdfPTable table = new PdfPTable(4); // 4 columns
+		table.setWidthPercentage(100);
+		table.setSpacingBefore(10f);
+		table.setSpacingAfter(10f);
 
-	    Font tableHeaderFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
-	    table.addCell(new Paragraph("Leave Type", tableHeaderFont));
-	    table.addCell(new Paragraph("Start Date", tableHeaderFont));
-	    table.addCell(new Paragraph("End Date", tableHeaderFont));
-	    table.addCell(new Paragraph("Days", tableHeaderFont));
+		Font tableHeaderFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
+		table.addCell(new Paragraph("Leave Type", tableHeaderFont));
+		table.addCell(new Paragraph("Start Date", tableHeaderFont));
+		table.addCell(new Paragraph("End Date", tableHeaderFont));
+		table.addCell(new Paragraph("Days", tableHeaderFont));
 
-	    for (EmpModel leave : leaveList) {
-	        table.addCell(leave.getLeave_type());
-	        table.addCell(leave.getFrom_date().toString());
-	        table.addCell(leave.getTo_date().toString());
-	        table.addCell(String.valueOf(leave.getDays()));
-	    }
+		for (EmpModel leave : leaveList) {
+			if (leave.isDeleted() == false) {
+				table.addCell(leave.getLeave_type());
+				table.addCell(leave.getFrom_date().toString());
+				table.addCell(leave.getTo_date().toString());
+				table.addCell(String.valueOf(leave.getDays()));
+			}
+		}
 
-	    document.add(table);
-	    document.add(Chunk.NEWLINE);
+		document.add(table);
+		document.add(Chunk.NEWLINE);
 	}
 
 }
--- backend/src/main/java/com/backend/backend/service/empserviceimpl/LeaveServiceImpl.java
+++ backend/src/main/java/com/backend/backend/service/empserviceimpl/LeaveServiceImpl.java
@@ -6,7 +6,10 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.List;
 
-import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DateUtil;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.util.NumberToTextConverter;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -19,11 +22,11 @@ import com.backend.backend.repository.LeaveRepository;
 import com.backend.backend.service.LeaveService;
 
 @Service
-public class LeaveServiceImpl implements LeaveService{
+public class LeaveServiceImpl implements LeaveService {
 
 	@Autowired
 	private LeaveRepository leaveRepository;
-	
+
 	@Override
 	public int addLeave(EmpModel empModel) {
 		return leaveRepository.addLeave(empModel);
@@ -56,81 +59,79 @@ public class LeaveServiceImpl implements LeaveService{
 
 	@Override
 	public void uploadFile(MultipartFile file) {
-		
-	try (InputStream inputStream = file.getInputStream();
-		Workbook workbook = new XSSFWorkbook(inputStream)) {
-		
-		Sheet sheet = workbook.getSheetAt(0);
-		for(Row row: sheet) {
-			if(row.getRowNum() == 0) {
-				continue;
+
+		try (InputStream inputStream = file.getInputStream(); Workbook workbook = new XSSFWorkbook(inputStream)) {
+
+			Sheet sheet = workbook.getSheetAt(0);
+			for (Row row : sheet) {
+				if (row.getRowNum() == 0) {
+					continue;
+				}
+
+				int emp_id = Integer.parseInt(getStringCellValue(row.getCell(0)));
+				String empName = getStringCellValue(row.getCell(1));
+				String nrc = getStringCellValue(row.getCell(2));
+				String phone = getStringCellValue(row.getCell(3));
+				String email = getStringCellValue(row.getCell(4));
+				String dob = getStringCellValue(row.getCell(5));
+				String rank = getStringCellValue(row.getCell(6));
+				String dep = getStringCellValue(row.getCell(7));
+				String address = getStringCellValue(row.getCell(8));
+				boolean checkDelete = Boolean.parseBoolean(getStringCellValue(row.getCell(9)));
+
+				int leaveId = Integer.parseInt(getStringCellValue(row.getCell(10)));
+				String leaveType = getStringCellValue(row.getCell(11));
+				String fromDate = getStringCellValue(row.getCell(12));
+				String toDate = getStringCellValue(row.getCell(13));
+				int days = Integer.parseInt(getStringCellValue(row.getCell(14)));
+				boolean deleted = Boolean.parseBoolean(getStringCellValue(row.getCell(15)));
+
+				EmpModel emp = new EmpModel();
+
+				emp.setEmp_id(emp_id);
+				emp.setEmp_name(empName);
+				emp.setNrc(nrc);
+				emp.setPhone(phone);
+				emp.setEmail(email);
+				emp.setDob(dob);
+				emp.setRank(rank);
+				emp.setDep(dep);
+				emp.setAddress(address);
+				emp.setCheckdelete(checkDelete);
+
+				emp.setLeave_id(leaveId);
+				emp.setLeave_type(leaveType);
+				emp.setFrom_date(fromDate);
+				emp.setTo_date(toDate);
+				emp.setDays(days);
+				emp.setDeleted(deleted);
+				leaveRepository.addEmployee(emp);
+				leaveRepository.addLeave(emp);
 			}
-			
-			int emp_id = Integer.parseInt(getStringCellValue(row.getCell(0)));
-			String empName = getStringCellValue(row.getCell(1));
-            String nrc = getStringCellValue(row.getCell(2));
-            String phone = getStringCellValue(row.getCell(3));
-            String email = getStringCellValue(row.getCell(4));
-            String dob = getStringCellValue(row.getCell(5));
-            String rank = getStringCellValue(row.getCell(6));
-            String dep = getStringCellValue(row.getCell(7));
-            String address = getStringCellValue(row.getCell(8));
-            boolean checkDelete = Boolean.parseBoolean(getStringCellValue(row.getCell(9)));
-			
-            int leaveId = Integer.parseInt(getStringCellValue(row.getCell(10)));
-            String leaveType = getStringCellValue(row.getCell(11));
-            String fromDate = getStringCellValue(row.getCell(12));
-            String toDate = getStringCellValue(row.getCell(13));
-            int days = Integer.parseInt(getStringCellValue(row.getCell(14)));
-            boolean deleted = Boolean.parseBoolean(getStringCellValue(row.getCell(15)));
-            
-            EmpModel emp = new EmpModel();
-			
-			emp.setEmp_id(emp_id);
-			emp.setEmp_name(empName);
-			emp.setNrc(nrc);
-			emp.setPhone(phone);
-			emp.setEmail(email);
-			emp.setDob(dob);
-			emp.setRank(rank);
-			emp.setDep(dep);
-			emp.setAddress(address);
-			emp.setCheckdelete(checkDelete);
-			
-			emp.setLeave_id(leaveId);
-            emp.setLeave_type(leaveType);
-            emp.setFrom_date(fromDate);
-            emp.setTo_date(toDate);
-            emp.setDays(days);
-            emp.setDeleted(deleted);
-             leaveRepository.addEmployee(emp);
-             leaveRepository.addLeave(emp);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-	} catch (IOException e) {
-		e.printStackTrace();
-	}			
-	}
-	
-	private String getStringCellValue(Cell cell) {
-	    if (cell == null) {
-	        return null;
-	    }
-	    switch (cell.getCellType()) {
-	        case STRING:
-	            return cell.getStringCellValue();
-	        case NUMERIC:
-	            // Check if the cell contains a date and format it accordingly
-	            if (DateUtil.isCellDateFormatted(cell)) {
-	                DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-	                return df.format(cell.getDateCellValue());
-	            } else {
-	                // Convert numeric value to string
-	                return NumberToTextConverter.toText(cell.getNumericCellValue());
-	            }
-	        default:
-	            return null;
-	    }
 	}
 
-	
+	private String getStringCellValue(Cell cell) {
+		if (cell == null) {
+			return null;
+		}
+		switch (cell.getCellType()) {
+		case STRING:
+			return cell.getStringCellValue();
+		case NUMERIC:
+			// Check if the cell contains a date and format it accordingly
+			if (DateUtil.isCellDateFormatted(cell)) {
+				DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+				return df.format(cell.getDateCellValue());
+			} else {
+				// Convert numeric value to string
+				return NumberToTextConverter.toText(cell.getNumericCellValue());
+			}
+		default:
+			return null;
+		}
+	}
+
 }
